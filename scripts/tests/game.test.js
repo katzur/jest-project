7573f4acaf8289c4e4a8a05160eeec8e4e9ace33
katzur <zurawska.kasia@gmail.com>
2022-05-28 10:46:59 +0000

    Add buttons to choices in js

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -29,5 +29,8 @@ describe("game object contains correct keys", () => {
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
     });
+    test("choices conain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
+    });
 });
 
